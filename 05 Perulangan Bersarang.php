6129450171a39711d7c1d6f825dd175f0d824476
aliyanovita <aliyaksp3@gmail.com>
2024-08-31 12:51:42 +0700

    05 Perulangan Bersarang.php

---
+++ 05 Perulangan Bersarang.php
@@ -0,0 +1,20 @@
+<?php
+for($i = 0; $i < 5; $i++){
+    for($j = 0; $j < 10; $j++){
+        echo "Ini perulangan ke ($i, $j)<br>";
+    }
+}
+?>
+
+Contoh lain:
+<?php
+$i = 0;
+while($i < 10){
+    for($j = 0; $j < 10; $j++){
+        echo "Ini perulangan ke ($i, $j)<br>";
+    }
+
+    $i++;
+}
+
+echo "<p><strong>by:aliya novita";
