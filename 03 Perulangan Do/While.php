bc30bb243d753aa9d8563e0e576cd0ab8e065b37
aliyanovita <aliyaksp3@gmail.com>
2024-08-31 11:23:41 +0700

    03 Perulangan Do/While.php

---
+++ 03 Perulangan Do/While.php
@@ -0,0 +1,15 @@
+<?php
+do {
+    // blok kode yang akan diulang    
+} while (<kondisi>);
+?>
+Contoh:
+<?php 
+
+$ulangi = 10;
+
+do {
+    echo "<p>ini adalah perulangan ke-$ulangi</p>";
+    $ulangi--;
+} while ($ulangi > 0);
+echo "<p><strong>by:aliya novita";
