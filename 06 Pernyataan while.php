4dbcc212a4085f74cf507eb844ca0c541aa09d29
aliyanovita <aliyaksp3@gmail.com>
2024-08-31 13:44:00 +0700

    06 Pernyataan white.php

--- 06 Pernyataan while.php
+++ 06 Pernyataan while.php
@@ -3,11 +3,12 @@
 while ($kondisi) {
     // Kode yang akan dijalankan
   }
-  Contoh penggunaan:
   
   $i = 1;
   while ($i <= 5) {
     echo "Nilai i: $i";
     $i++;
   }
-  echo "<p><strong>by:aliya novita";  
+  echo "<p><strong>by:aliya novita";
+  
+  
