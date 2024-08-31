dfe6f6d2f4d98f4a67fd5a80f8c8c509d7d0581c
aliyanovita <aliyaksp3@gmail.com>
2024-08-31 12:56:52 +0700

    06 Pernyataan while.php

---
+++ 06 Pernyataan while.php
@@ -0,0 +1,13 @@
+<?php
+
+while ($kondisi) {
+    // Kode yang akan dijalankan
+  }
+  Contoh penggunaan:
+  
+  $i = 1;
+  while ($i <= 5) {
+    echo "Nilai i: $i";
+    $i++;
+  }
+  echo "<p><strong>by:aliya novita";  
