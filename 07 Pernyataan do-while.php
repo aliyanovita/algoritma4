bb30d0a44f43b56d20022bc2b9660e39b6f7457e
aliyanovita <aliyaksp3@gmail.com>
2024-08-31 13:48:17 +0700

    07 Pernyataan do-white.php

---
+++ 07 Pernyataan do-while.php
@@ -0,0 +1,14 @@
+<?php
+do-while serupa dengan while, tetapi memastikan bahwa kode dijalankan setidaknya satu kali.
+
+do {
+  // Kode yang akan dijalankan
+} while ($kondisi);
+
+$i = 1;
+do {
+  echo "Nilai i: $i";
+  $i++;
+} while ($i <= 5);
+echo "<p><strong>by:aliya novita";
+
