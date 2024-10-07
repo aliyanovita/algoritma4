546085cbb5bcb79c66dad16a3dca8afc8deea304
aliyanovita <aliyaksp3@gmail.com>
2024-10-07 11:04:54 +0700

    20 array assosiatif.php

---
+++ 20 array assosiatif.php
@@ -0,0 +1,27 @@
+<?php
+// membuat array asosiatif
+$artikel = [
+    "judul" => "Belajar Pemrograman PHP",
+    "penulis" => "ahmadimuslim",
+    "view" => 128
+];
+
+// mencetak isi array assosiatif
+echo "<h2>".$artikel["judul"]."</h2>";
+echo "<p>oleh: ".$artikel["penulis"]."</p>";
+echo "<p>View: ".$artikel["view"]."</p>";
+
+echo "<hr>";
+
+
+$email["subjek"] = "Apa Kabar?";
+$email["pengirim"] = "dian@ahmadimuslim.com";
+$email["isi"] = "Apa kabar? sudah lama tidak berjumpa";
+
+echo "<pre>";
+print_r($email);
+echo "</pre>";
+
+echo "<hr>";
+$nama_variabel=array("nama1"=>"70","nama2"=>"67","nama3"=>"89");
+echo "hasil dari program " . $nama_variabel['nama1'] . " nilai.";
